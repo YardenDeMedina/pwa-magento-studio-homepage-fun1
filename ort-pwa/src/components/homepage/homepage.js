6ef2ece4c92919de678bf613c64fb4c23163dd1d
jordan <yard@gmail.com>
2021-06-24 15:45:30 +0300

    5

--- ort-pwa/src/components/homepage/homepage.js
+++ ort-pwa/src/components/homepage/homepage.js
@@ -29,6 +29,54 @@ const Homepage = (props) => {
 
     return (
     <div className={classes.main_class}>
+
+      <div>
+        <h1>CSS-only Carousel</h1>
+        <p>This carousel is created with HTML and CSS only.</p>
+        <section className={classes.carousel} aria-label="Gallery">
+          <ol className={classes.carousel__viewport}>
+            <li id="carousel__slide1" tabIndex={0} className={classes.carousel__slide}>
+              <div className={classes.carousel__snapper}>
+                <a href="#carousel__slide4" className={classes.carousel__prev}>Go to last slide</a>
+                <a href="#carousel__slide2" className={classes.carousel__next}>Go to next slide</a>
+              </div>
+            </li>
+            <li id="carousel__slide2" tabIndex={0} className={classes.carousel__slide}>
+              <div className={classes.carousel__snapper} />
+              <a href="#carousel__slide1" className={classes.carousel__prev}>Go to previous slide</a>
+              <a href="#carousel__slide3" className={classes.carousel__next}>Go to next slide</a>
+            </li>
+            <li id="carousel__slide3" tabIndex={0} className={classes.carousel__slide}>
+              <div className={classes.carousel__snapper} />
+              <a href="#carousel__slide2" className={classes.carousel__prev}>Go to previous slide</a>
+              <a href="#carousel__slide4" className={classes.carousel__next}>Go to next slide</a>
+            </li>
+            <li id="carousel__slide4" tabIndex={0} className={classes.carousel__slide}>
+              <div className={classes.carousel__snapper} />
+              <a href="#carousel__slide3" className={classes.carousel__prev}>Go to previous slide</a>
+              <a href="#carousel__slide1" className={classes.carousel__next}>Go to first slide</a>
+            </li>
+          </ol>
+          <aside className={classes.carousel__navigation}>
+            <ol className={classes.carousel__navigation-list}>
+              <li className={classes.carousel__navigation-item}>
+                <a href="#carousel__slide1" className="carousel__navigation-button">Go to slide 1</a>
+              </li>
+              <li className={classes.carousel__navigation-item}>
+                <a href="#carousel__slide2" className="carousel__navigation-button">Go to slide 2</a>
+              </li>
+              <li className={classes.carousel__navigation-item}>
+                <a href="#carousel__slide3" className="carousel__navigation-button">Go to slide 3</a>
+              </li>
+              <li className={classes.carousel__navigation-item}>
+                <a href="#carousel__slide4" className="carousel__navigation-button">Go to slide 4</a>
+              </li>
+            </ol>
+          </aside>
+        </section>
+      </div>
+
+
         {data.products.items.map(product => {
             return (
               <React.Fragment>
