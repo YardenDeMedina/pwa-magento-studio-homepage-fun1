f891e9043d7242d5a968f0a72e47e2415f71c00c
jordan <yard@gmail.com>
2021-06-25 13:45:58 +0300

    5

--- ort-pwa/src/components/homepage/main.js
+++ ort-pwa/src/components/homepage/main.js
@@ -16,6 +16,14 @@ function MainHomePageComp(props){
             {/* Box at the begnnning of the page */}
             <div className={classes.box}>Yarden's</div> 
 
+            {/* IMPORTANT COMMENT: */}
+
+            {/* My plan was to create one ProductComp and write it three times (instead of these comps) 
+            and send the data from GraphQl and the cataegory name as props (or use redux)... 
+            but I did not know how to make such a GraphQl request that will 
+            sort the information into different arrays for each category...  
+            so for now I made a gql request from each product component */}
+
             <SweatersComp/>
             <DressesComp/>
             <SkirtsComp/>
